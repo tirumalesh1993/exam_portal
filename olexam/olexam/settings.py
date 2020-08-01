30962e0482dac691b06f55dc3a8bdb98034d901b
Ubuntu <tirumalesh@olexam.n1mrfgvmmr2uneiy4eclddikla.hx.internal.cloudapp.net>
2020-08-01 17:59:06 +0000

    code updated

--- olexam/olexam/settings.py
+++ olexam/olexam/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-9n!(i!e@&i$f1&rd*#c91_0f4^tu8)@=mu$mjgtymv9!f1!26'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
